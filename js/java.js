891fec6052a0525f5beb1f1c10c4aa81861284a3
DDBalazs <csokebalazszsolt99@gmail.com>
2024-12-04 10:27:37 +0100

    first commit

---
+++ js/java.js
@@ -0,0 +1,47 @@
+function balf() {
+    document.getElementById("szoveg").style.cssText = "text-align : left; margin-top"
+}
+function kozepf() {
+    document.getElementById("szoveg").style.cssText = "text-align : center; margin-top"
+}
+function jobbf() {
+    document.getElementById("szoveg").style.cssText = "text-align : right; margin-top"
+}
+function balk() {
+    document.getElementById("szoveg").style.cssText = "left: 0; right: auto; top: 50%; bottom: auto"
+}
+function kozepk() {
+    document.getElementById("szoveg").style.cssText = "left: 50%; right: auto; top: 50%; bottom: auto"
+}
+function jobbk() {
+    document.getElementById("szoveg").style.cssText = "text-align : right; margin: auto"
+}
+function ball(){
+    document.getElementById("szoveg").style.cssText = "text-align: left; margin-bottom"
+}
+function kozepl(){
+    document.getElementById("szoveg").style.cssText = "text-align: center; margin-bottom"
+}
+function jobbl(){
+    document.getElementById("szoveg").style.cssText = "text-align: right; margin-bottom"
+}
+
+function invisible(){
+    document.getElementById("szoveg").textContent = "";
+}
+function visible(){
+    document.getElementById("szoveg").textContent = "Lorem ipsum dolor sit amet consectetur adipisicing elit.";
+}
+
+function kek(){
+    document.getElementById("szoveg").style.cssText = "color: blue"
+}
+function piros(){
+    document.getElementById("szoveg").style.cssText = "color: green"
+}
+function zold(){
+    document.getElementById("szoveg").style.cssText = "color: red"
+}
+function normal(){
+    document.getElementById("szoveg").style.cssText = "color: black"
+}
